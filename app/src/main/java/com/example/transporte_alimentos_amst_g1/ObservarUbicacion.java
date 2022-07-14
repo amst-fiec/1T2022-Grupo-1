f13a036cc656698372172422acbe79aa87d35f9a
Alex Mora <aleemora@espol.edu.ec>
2022-07-14 15:46:50 -0500

    Se crearon las Activities para las tres pantallas que faltaban, ademas se las conectaron a la aplicacion por medio de botones en el menu principal

---
+++ app/src/main/java/com/example/transporte_alimentos_amst_g1/ObservarUbicacion.java
@@ -0,0 +1,14 @@
+package com.example.transporte_alimentos_amst_g1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ObservarUbicacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_observar_ubicacion);
+    }
+}
