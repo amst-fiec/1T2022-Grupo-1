e6dced5f2c290236192355e7fac657f430ff038b
Alex Mora <aleemora@espol.edu.ec>
2022-08-25 19:31:18 -0500

    Corrección de errores y sugerencia por parte del code inspector

--- app/src/main/java/com/example/transporte_alimentos_amst_g1/clases/ClaseUsando.java
+++ app/src/main/java/com/example/transporte_alimentos_amst_g1/clases/ClaseUsando.java
@@ -1,4 +1,5 @@
 package com.example.transporte_alimentos_amst_g1.clases;
+import com.example.transporte_alimentos_amst_g1.clases.usuario;
 
 public class ClaseUsando {
     //clase estatica para guardar el ingreso de los usuarios en el programa.
